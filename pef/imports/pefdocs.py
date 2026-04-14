0a18ad474e2546b40c7d8df9e47f0353ffd7e077
Rafal Janicki <bloorq@gmail.com>
2026-04-14 21:20:18 +0100

    updates

--- pef/imports/pefdocs.py
+++ pef/imports/pefdocs.py
@@ -20,6 +20,7 @@ exploitableFunctions = {
         "Function(",
         "textContent",
         "innerText",
+        "URLSearchParams(",
     ],
     "php": [
         # "`",
@@ -253,7 +254,15 @@ exploitableFunctionsDesc = {
             "XSS, HTML Injection, Content Injection etc.",
             "medium",
             "sink"
-        ]
+        ],
+        "URLSearchParams": [
+            "The URLSearchParams interface defines utility methods to work with the query string of a URL.",
+            "URLSearchParams()",
+            "XSS, HTML Injection, Content Injection etc.",
+            "medium",
+            "source"
+        ],
+
     },
     "php": {
         "`": [
